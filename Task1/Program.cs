c12fdccaa5ec0462a807385e83520ee14bb5deb9
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-15 10:54:25 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int inputNumber = new Random().Next(100, 1000);
+
+int digit2 = GetDigit2(inputNumber);
+
+Console.WriteLine($"Вторая цифра числа {inputNumber}: {digit2}");
+
+int GetDigit2(int input)
+{
+    return input / 10 % 10;
+}
