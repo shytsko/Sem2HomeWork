f1ba3b9ee90a00408d03fa3f8725560a49dcaf11
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-17 13:31:25 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Доп. задача: Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает.
+// Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше
+// Доп. задача с усложнением: на отгадывание дается 3 попытки.
+
+int inputMysteryNumber = new Random().Next(100);
+
+Console.WriteLine($"Загадоное число: {inputMysteryNumber}");
+
+Console.WriteLine("Отгадайте загадонное число от 0 до 99");
+
+int countTries = 3;
+while (countTries > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Осталось попыток: {countTries}");
+    Console.Write("Введите число: ");
+    int inputUsersNumber = Convert.ToInt32(Console.ReadLine());
+    if (inputUsersNumber == inputMysteryNumber)
+        break;
+    else
+    {
+        Console.Write("Не угадали. Ваше число ");
+        if (inputUsersNumber > inputMysteryNumber)
+            Console.Write("больше ");
+        else
+            Console.Write("меньше ");
+        Console.WriteLine("загадонного");
+    }
+    countTries--;
+}
+
+if (countTries > 0)
+    Console.WriteLine("Поздравляем!!! Вы угадали число!!!");
+else
+    Console.WriteLine("Увы. Больше попыток нет");
+
